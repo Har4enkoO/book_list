98f1e7977cc690ea2cb2cc9b18f9461b77048271
Har4enkoO <pan4enko48@gmail.com>
2022-04-26 19:29:00 +0300

    update components

--- Desktop/inventor_Academy/bem-css/src/components/card/card.jsx
+++ Desktop/inventor_Academy/bem-css/src/components/card/card.jsx
@@ -4,10 +4,10 @@ import './style.css'
 
 export const Card = () => {
   return (
-    <article className="main__article">
-      <div className="main__article__wrapper">
-        <h2 className="main__article__title">Webdesing Agentur</h2>
-        <p className="main__article__title__p">
+    <article className="main__card">
+      <div className="main__card__wrapper">
+        <h2 className="main__card__title">Webdesing Agentur</h2>
+        <p className="main__card__text">
           Wir sind ein wirksames Team von Programmiern und Designer und bieten
           professionelle Dienstlei stungen rund um's Thema Webdesing und
           Online-Marketing an. Seint 2006 kultivieren wir
